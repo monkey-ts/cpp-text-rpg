c1d3a001e375f29a21f1dd7a2f389a88f044ae9c
monkey-ts <m.inifiedcode@gmail.com>
2022-10-13 13:58:56 -0500

    Add a skeleton for player manager class.

---
+++ PlayerManager.h
@@ -0,0 +1,13 @@
+#ifndef PLAYERMANAGER_H
+#define PLAYERMANAGER_H
+
+#include <iostream>
+
+class PlayerManager {
+public:
+	void getTest() {
+		std::cout << "Got your test, bud." << std::endl;
+	}
+};
+
+#endif
